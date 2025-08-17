573340134feaf697845dd93b42df1fa6ed2d1998
cyancoderix <cyancoderix@proton.me>
2025-08-17 19:23:00 +0200

    Make package (module) accessible from other used modules Package now needs path to it

--- init.lua
+++ init.lua
@@ -6,8 +6,10 @@
 -- See the license in the LICENSE
 -- ================================
 
-package.path = package.path..";../?.lua"
-
-require("TableUtils.actions")
-require("TableUtils.queries")
-require("TableUtils.iterators")
+return function(path)
+	if path[#path] == "/" then path = path:substring(1,#path-1) end
+	package.path = package.path..";"..path.."/../?.lua"
+	require("TableUtils.actions")
+	require("TableUtils.queries")
+	require("TableUtils.iterators")
+end
